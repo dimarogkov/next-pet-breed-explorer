e344619c3590c86781ad3ebb42f54c95cd3e125b
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-08-08 18:44:09 +0300

    add header

---
+++ src/components/elements/Header/Header.tsx
@@ -0,0 +1,13 @@
+import HeaderLogo from '../HeaderLogo/HeaderLogo';
+
+const Header = () => {
+    return (
+        <header className='relative flex items-center w-full h-16 lg:h-20 border-b border-gray'>
+            <div className='w-full max-w-screen-2xl px-4 sm:px-5 m-auto'>
+                <HeaderLogo />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
