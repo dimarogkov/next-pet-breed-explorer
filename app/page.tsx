5b7b5f0045c285907e35a5655b2b0cc09fea026d
dima.rogkov <dimarogkov41475@ukr.net>
2024-08-08 17:46:11 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { BtnLink, Text, Title } from '@/src/components/ui';
+
+const HomePage = () => {
+    return (
+        <section className='relative w-full'>
+            <div className='w-full mb-5 last:mb-0'>
+                <Title className='mb-2 last:mb-0'>Main Title</Title>
+                <Text className='mb-5 last:mb-0'>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium non commodi earum. Minus
+                    numquam iure, aliquid esse quae vitae. Voluptas, magni laudantium! Eos sapiente similique officiis
+                    expedita distinctio aliquam quos!
+                </Text>
+                <BtnLink href='/'>Button Link</BtnLink>
+            </div>
+        </section>
+    );
+};
+
+export default HomePage;
---
+++ src/components/ui/Btn.tsx
@@ -0,0 +1,24 @@
+import { ButtonHTMLAttributes, forwardRef } from 'react';
+import cn from 'classnames';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+    btnType?: string;
+    className?: string;
+}
+
+const Btn: React.FC<Props> = forwardRef<HTMLButtonElement, Props>(({ className = '', ...props }, ref) => (
+    <button
+        ref={ref}
+        {...props}
+        className={cn(
+            `flex items-center justify-center w-full sm:w-fit sm:min-w-32 lg:min-w-36 h-10 lg:h-11 font-media px-4 rounded text-white transition-opacity duration-300 hover:opacity-80 ${className}`,
+            {
+                'pointer-events-none bg-gray': props.disabled,
+                'bg-blue': !props.disabled,
+            }
+        )}
+    />
+));
+
+Btn.displayName = 'Btn';
+export default Btn;
